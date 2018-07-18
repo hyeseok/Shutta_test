3d00934c5728c384ea8f0dcf1d690815fea81740
hyeseok <41276412+hyeseok@users.noreply.github.com>
2018-07-18 17:13:48 +0900

    Dealer 중간 부분까지 추가변경, Card 기본필드 변경 추가

--- Shutta_test/Shutta_test/Card.cs
+++ Shutta_test/Shutta_test/Card.cs
@@ -7,5 +7,7 @@ namespace Shutta_test
 {
     public class Card
     {
+        public int number; // 섯다의 10가지 카드를 숫자로 표현하기 위한 변수 1~10
+        public bool isKwang; // 카드가 광땡인 카드인지를 판별하기 위한 변수
     }
 }
--- Shutta_test/Shutta_test/Dealer.cs
+++ Shutta_test/Shutta_test/Dealer.cs
@@ -7,5 +7,24 @@ namespace Shutta_test
 {
     public class Dealer
     {
+        private List<Card> cards = new List<Card>(); // 카드를 받아와서 List배열에 담기
+        private List<int> kwangNumbers = new List<int> { 1, 3, 8 };
+        // isKwang이라는 List배열에 {1,3,8}을 담아 1,8광땡 / 3,8광땡을 표현하기 위함
+
+        // 카드 섞기 구현
+        public void ShuffleCard()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Card card = new Card();
+                    card.number = i + 1;
+                    card.isKwang = false;
+                }
+            }
+        }
+
+        // 카드 1장씩 나눠주기
     }
 }
